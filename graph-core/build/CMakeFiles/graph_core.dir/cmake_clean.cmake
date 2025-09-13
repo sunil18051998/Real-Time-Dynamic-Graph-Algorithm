1a8ade19737366761f570b9e54ac26065979be03
Sunil Maurya <mauryasunilkumar98@gmail.com>
2025-09-13 14:53:40 +0530

    remove

---
+++ graph-core/build/CMakeFiles/graph_core.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graph_core.dir/src/Graph.cpp.obj"
+  "CMakeFiles/graph_core.dir/src/Graph.cpp.obj.d"
+  "CMakeFiles/graph_core.dir/src/main.cpp.obj"
+  "CMakeFiles/graph_core.dir/src/main.cpp.obj.d"
+  "graph_core.exe"
+  "graph_core.exe.manifest"
+  "graph_core.lib"
+  "graph_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graph_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
